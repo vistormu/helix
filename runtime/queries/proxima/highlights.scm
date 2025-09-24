06f6e999f17466e135e09312397fffc9a0f6e64e
vistor <vistormu@gmail.com>
2025-09-24 11:13:50 +0200

    latest update

---
+++ runtime/queries/proxima/highlights.scm
@@ -0,0 +1,12 @@
+[
+  (escaped_char) @type
+  (tag_at) @tag
+  (arg_name) @string
+  (identifier) @function
+  (argument) @parameter
+  (comment) @comment
+  ; (text) @text
+  (comment) @spell
+  (text) @spell
+]
+
